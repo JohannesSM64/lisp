a59a8b4d259dcd01a54660470eaa3a5599151540
JohannesSM64 <johannes@sm64.org>
2013-12-15 22:47:41 +0100

    Revamped the imperative part

--- yatzy.lisp
+++ yatzy.lisp
@@ -1,12 +1,12 @@
 ;;; Gosu Yatzy
 ;;; Written by Johannes Langøy, December 2013
-(load "utils")
+(load "source/utils")
 
 (defun numbers (n dice)
   (and (<= 1 (count n dice))
        (* n (count n dice))))
 
-(defun pair (dice)
+(defun one-pair (dice)
   (let ((s nil))
     (dolist (n '(6 5 4 3 2 1))
       (when (<= 2 (count n dice))
@@ -26,7 +26,7 @@
                                             (* (car x) (cadr x))))
                                         l))))))
 
-(defun three-alike (dice)
+(defun three-of-a-kind (dice)
   (let ((s nil))
     (dolist (n '(6 5 4 3 2 1))
       (when (<= 3 (count n dice))
@@ -36,7 +36,7 @@
         (return-from nil)))
     s))
 
-(defun four-alike (dice)
+(defun four-of-a-kind (dice)
   (let ((s nil))
     (dolist (n '(6 5 4 3 2 1))
       (when (<= 4 (count n dice))
@@ -56,7 +56,7 @@
                               '(2 3 4 5 6)))
     20))
 
-(defun full-house (dice)
+(defun house (dice)
   (let* ((l (mapcar (lambda (n) (list n (count n dice)))
                     '(1 2 3 4 5 6)))
          (lc (mapcar #'cadr l)))
@@ -75,45 +75,78 @@
     50))
 
 (defun check-all (dice)
-  `((ones ,(numbers 1 dice))
-    (twos ,(numbers 2 dice))
-    (threes ,(numbers 3 dice))
-    (fours ,(numbers 4 dice))
-    (fives ,(numbers 5 dice))
-    (sixes ,(numbers 6 dice))
-    (pair ,(pair dice))
-    (two-pairs ,(two-pairs dice))
-    (three-alike ,(three-alike dice))
-    (four-alike ,(four-alike dice))
-    (small-straight ,(small-straight dice))
-    (large-straight ,(large-straight dice))
-    (full-house ,(full-house dice))
-    (chance ,(chance dice))
-    (yatzy ,(yatzy dice))))
+  `((ones "Ones" ,(numbers 1 dice))
+    (twos "Twos" ,(numbers 2 dice))
+    (threes "Threes" ,(numbers 3 dice))
+    (fours "Fours" ,(numbers 4 dice))
+    (fives "Fives" ,(numbers 5 dice))
+    (sixes "Sixes" ,(numbers 6 dice))
+    (one-pair "One pair" ,(one-pair dice))
+    (two-pairs "Two pairs" ,(two-pairs dice))
+    (three-of-a-kind "Three of a kind" ,(three-of-a-kind dice))
+    (four-of-a-kind "Four of a kind" ,(four-of-a-kind dice))
+    (small-straight "Small straight" ,(small-straight dice))
+    (large-straight "Large straight" ,(large-straight dice))
+    (house "House" ,(house dice))
+    (chance "Chance" ,(chance dice))
+    (yatzy "Yatzy" ,(yatzy dice))))
 
 (defun remove-unfulfilled (checks)
-  (remove-if (lambda (x) (null (cadr x)))
+  (remove-if (lambda (x) (null (third x)))
              checks))
 
 ;; Imperative part
 (defun roll-dice (amount)
   (loop repeat amount collect (1+ (random 6))))
 
+(defun roll-unkept-dice (dice)
+  (append dice (roll-dice (- 5 (length dice)))))
+
+(defun one-turn ()
+  (let ((dice nil) (n 1))
+    (loop named turn repeat 3 do
+          (setf dice (roll-unkept-dice dice))
+          (format t "Roll ~a: " n)
+          (princ dice)
+          (fresh-line)
+          (unless (= 3 n)
+            (princ "Enter which dice to keep: ")
+            (setf dice (read))
+            (incf n))
+          (if (= 5 (length dice))
+            (return-from turn)))
+    dice))
+
 (defun game-loop ()
   (defvar *score* 0)
-  (setf *dice* nil)
-  (setf *checks* nil)
-  (format t "Welcome to Gosu Yatzy.~%")
-  (format t "Hit RET to roll your dice.~%")
-  (read-line)
-  (loop repeat 2 do
-        (setf *dice* (append *dice* (roll-dice (- 5 (length *dice*)))))
-        (princ *dice*)
-        (fresh-line)
-        (format t "Enter which dice to keep.~%")
-        (setf *dice* (read *standard-input*))
-        (if (= 5 (length *dice*))
-          (return-from nil)))
-  (setf *dice* (append *dice* (roll-dice (- 5 (length *dice*)))))
-  (princ *dice*)
-  (setf *checks* (remove-unfulfilled (check-all *dice*))))
+  (defvar *checked-boxes* nil)
+  (loop named game do
+        ;; Interactively roll the dice.
+        (setf *dice* (one-turn))
+        ;; Get a list of choices for what do do with these dice.
+        (setf *choices* (remove-unfulfilled (check-all *dice*)))
+        ;; Delete the choices that are already used.
+        (dolist (x *checked-boxes*)
+          (delete-if (lambda (y) (eq (first y) (first x)))
+                     *choices*))
+        ;; Print the choices.
+        (dolist (x *choices*)
+          (format t "~a: ~a~%" (second x) (third x)))
+        ;; Ask which choice to use.
+        (loop named check do
+              (format t "Check which box? ")
+              (setf *selection* (read))
+              (if (assoc *selection* *choices*)
+                (return-from check)
+                (if (y-or-n-p "Cross out ~a? " *selection*)
+                  (return-from check))))
+        ;; Extract the points, if any.
+        (setf *points* (third (assoc *selection* *choices*)))
+        ;; Update the list of checked boxes.
+        (push (list *selection* *points*) *checked-boxes*)
+        ;; Update the score.
+        (and *points* (incf *score* *points*))
+        ;; Check if it's time to end the game.
+        (when (= (length *checked-boxes*) 15)
+          (format t "Game over. Final score: ~a~%" *score*)
+          (return-from game))))
