58e1273ef109510b12d0f79f223369a13ca5e191
JohannesSM64 <johannes@sm64.org>
2014-01-13 19:58:16 +0100

    more redesign

--- yatzy.lisp
+++ yatzy.lisp
@@ -1,78 +1,65 @@
 ;;; Gosu Yatzy
 ;;; Written by Johannes Langøy, December 2013 - January 2014
 
+(load "utils")
+
 (labels ((numfunc (n)
            (lambda (dice)
              (if (<= 1 (count n dice))
                (* n (count n dice))))))
   (defvar *goals*
-    (list ('ones "Ones" (numfunc 1))
-          ('twos "Twos" (numfunc 2))
-          ('threes "Threes" (numfunc 3))
-          ('fours "Fours" (numfunc 4))
-          ('fives "Fives" (numfunc 5))
-          ('sixes "Sixes" (numfunc 6))
-          ('one-pair
-           "One pair"
-           (lambda (dice)
-             (loop for n from 6 downto 1 do
-                   (when (<= 2 (count n dice))
-                     (return-from nil (* n 2))))))
-          ('two-pairs
-           "Two pairs"
-           (lambda (dice)
-             (let ((l (remove-if (lambda (n) (< (count n dice) 2))
-                                 '(1 2 3 4 5 6))))
-               (if (= 2 (length l))
-                 (apply #'+ (mapcar (lambda (n) (* 2 n)) l))))))
-          ('three-of-a-kind
-           "Three of a kind"
-           (lambda (dice)
-             (loop for n from 6 downto 1 do
-                   (when (<= 3 (count n dice))
-                     (return-from nil (* n 3))))))
-          ('four-of-a-kind
-           "Four of a kind"
-           (lambda (dice)
-             (loop for n from 6 downto 1 do
-                   (when (<= 4 (count n dice))
-                     (return-from nil (* n 4))))))
-          ('small-straight
-           "Small straight"
-           (lambda (dice)
-             (unless (member nil (loop for n from 1 to 5
-                                       collect (find n dice)))
-               15)))
-          ('large-straight
-           "Large straight"
-           (lambda (dice)
-             (unless (member nil (loop for n from 2 to 6
-                                       collect (find n dice)))
-               20)))
-          ('house
-           "House"
-           (lambda (dice)
-             (let ((l (loop for n from 1 to 6 collect (count n dice))))
-               (let ((two   (position 2 l))
-                     (three (position 3 l)))
-                 (and two three
-                      (+ (* 2 (1+ two))
-                         (* 3 (1+ three))))))))
-          ('chance
-           "Chance"
-           (lambda (dice)
-             (apply #'+ dice)))
-          ('yatzy
-           "Yatzy"
-           (lambda (dice)
-             (when (apply #'= dice)
-               50))))))
-
-(defun goal-string (g)
-  (second (assoc g *goals*)))
-
-(defun goal-function (g)
-  (third (assoc g *goals*)))
+    `(("Ones" ,(numfunc 1))
+      ("Twos" ,(numfunc 2))
+      ("Threes" ,(numfunc 3))
+      ("Fours" ,(numfunc 4))
+      ("Fives" ,(numfunc 5))
+      ("Sixes" ,(numfunc 6))
+      ("One pair"
+       ,(lambda (dice)
+         (loop for n from 6 downto 1 do
+               (when (<= 2 (count n dice))
+                 (return-from nil (* n 2))))))
+      ("Two pairs"
+       ,(lambda (dice)
+         (let ((l (remove-if (lambda (n) (< (count n dice) 2))
+                             '(1 2 3 4 5 6))))
+           (if (= 2 (length l))
+             (apply #'+ (mapcar (lambda (n) (* 2 n)) l))))))
+      ("Three of a kind"
+       ,(lambda (dice)
+         (loop for n from 6 downto 1 do
+               (when (<= 3 (count n dice))
+                 (return-from nil (* n 3))))))
+      ("Four of a kind"
+       ,(lambda (dice)
+         (loop for n from 6 downto 1 do
+               (when (<= 4 (count n dice))
+                 (return-from nil (* n 4))))))
+      ("Small straight"
+       ,(lambda (dice)
+         (unless (member nil (loop for n from 1 to 5
+                                   collect (find n dice)))
+           15)))
+      ("Large straight"
+       ,(lambda (dice)
+         (unless (member nil (loop for n from 2 to 6
+                                   collect (find n dice)))
+           20)))
+      ("House"
+       ,(lambda (dice)
+         (let ((l (loop for n from 1 to 6 collect (count n dice))))
+           (let ((two   (position 2 l))
+                 (three (position 3 l)))
+             (and two three
+                  (+ (* 2 (1+ two))
+                     (* 3 (1+ three))))))))
+      ("Chance"
+       ,(lambda (dice)
+         (apply #'+ dice)))
+      ("Yatzy"
+       ,(lambda (dice)
+         (when (apply #'= dice)
+           50))))))
 
 ;; Imperative part
 (defun game-loop ()
@@ -119,33 +106,26 @@
                 (if (= 5 (length dice))
                   (return-from turn dice)))
           ;; Initialize choices.
-          (setq choices *goals*)
+          (setq choices (mapcar #'car *goals*))
           ;; Remove the choices that are already used.
           (dolist (x boxes)
-            (setq choices (remove-if (lambda (y) (eq (car x) (car y)))
+            (setq choices (remove-if (lambda (y) (eq (car x) y))
                                      choices)))
           ;; Get our choices.
-          (setq choices (mapcar (lambda (g)
-                                  (cons (car g)
-                                        (funcall (goal-function (car g))
-                                                 dice)))
-                                choices))
+          (setq choices
+                (mapcar (lambda (g)
+                          (cons g
+                                (funcall (second (assoc g *goals*))
+                                         dice)))
+                        choices))
           ;; Separate fulfilled and unfulfilled goals.
-          (setq cross-choices (mapcar #'car
-                                      (remove-if (lambda (x) (cdr x))
-                                                 choices)))
-          (setq choices (remove-if (lambda (x) (null (cdr x)))
-                                   choices))
+          (setq cross-choices (flatten (remove-if #'cdr choices)))
+          (setq choices (remove-if #'single choices))
           ;; Print the choices.
           (loop for i in choices and n from 1 do
-                (format t "~a) ~a: ~a~%"
-                        n
-                        (goal-string (car i))
-                        (cdr i)))
+                (format t "~a) ~a: ~a~%" n (car i) (cdr i)))
           (loop for i in cross-choices and n from 0 do
-                (format t "~a) __~a__~%"
-                        (code-char (+ 65 n))
-                        (goal-string i)))
+                (format t "~a) __~a__~%" (code-char (+ 65 n)) i))
           ;; Get a selection.
           (setq selection nil)
           (loop named check do
@@ -172,7 +152,9 @@
           (when (cdr selection) ; not crossing
             (incf score (cdr selection))
             (when (member (car selection)
-                          '(ones twos threes fours fives sixes))
+                          '("Ones" "Twos" "Threes"
+                            "Fours" "Fives" "Sixes")
+                          :test #'equal)
               (progn (incf bonus (cdr selection))
                      (format t "Bonus: ~a/63~%" bonus))))
           ;; Check if it's time to end the game.
@@ -183,7 +165,7 @@
             (dolist (x (mapcar (lambda (x) (assoc x boxes))
                                (mapcar #'first *goals*)))
               (format t "~a: ~a~%"
-                      (goal-string x)
+                      x
                       (or (cdr x) "--")))
             (if (>= bonus 63)
               (incf score 50))
