6bbf7264ecafe7b785df74515856ef79789a2dbb
JohannesSM64 <johannes@sm64.org>
2014-01-13 19:56:53 +0100

    some updates

--- utils.lisp
+++ utils.lisp
@@ -54,11 +54,19 @@
 ;; }}}
 
 ;; On Lisp {{{
+(proclaim '(inline last1 single append1 conc1 mklist))
+
 (defun last1 (lst)
   (car (last lst)))
 
-(defun append1 (obj lst)
+(defun single (lst)
+  (and (consp lst) (not (cdr lst))))
+
+(defun append1 (lst obj)
   (append lst (list obj)))
+
+(defun conc1 (lst obj)
+  (nconc lst (list obj)))
 
 (defun mklist (obj)
   (if (listp obj) obj (list obj)))
@@ -95,12 +103,11 @@
     (rec tree nil)))
 
 (defun find2 (fn lst)
-  (if (null lst)
-    nil
-    (let ((val (funcall fn (car lst))))
-      (if val
-        (values (car lst) val)
-        (find2 fn (cdr lst))))))
+  (and lst
+       (let ((val (funcall fn (car lst))))
+         (if val
+           (values (car lst) val)
+           (find2 fn (cdr lst))))))
 
 (defun before (x y lst &key (test #'eql))
   (and lst
