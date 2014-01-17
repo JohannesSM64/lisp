0f04a6044f38738b0ed03ee02df2c0e14478cf3f
JohannesSM64 <johannes@sm64.org>
2014-01-17 21:21:01 +0100

    more

--- typing.lisp
+++ typing.lisp
@@ -7,11 +7,12 @@
 (defun test ()
   (let ((cleared 0)
         (lines (shuffle *lines*)))
-    (unwind-protect
       (dolist (l lines)
         (prinf l)
         (if (equal (read-line) l)
           (progn (incf cleared)
-                 (format t "=== Correct! ===~%" cleared))
-          (prinf "=== Failed! ==="))))
-    (prinf cleared)))
+                 (prinf "=== Correct! ==="))
+          (prinf "=== Failed! ===")))
+      (format t "You cleared ~a out of ~a lines.~%"
+                cleared
+                (length lines))))
