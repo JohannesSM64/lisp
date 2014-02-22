e540d05f6801b93218d74cdb65eaf62a63b3443b
JohannesSM64 <johannes@sm64.org>
2014-02-22 19:44:54 +0100

    some changes

--- shell.lisp
+++ shell.lisp
@@ -1,16 +1,32 @@
+;; Current features:
+;; - Basic line editing, history and file name completion (readline)
+;; - Multi-word arguments with '"
+;; - Escape the next character with \
+
+(defun push-char (chr str)
+  (concatenate 'string str (string chr)))
+
 (defun parse (line)
-  (let (result (acc "") inquotes)
+  (let (result (acc "") inquotes backslashed)
     (loop for c across line do
           (case c
-            (#\Space
-             (if inquotes
-                 (setq acc (concatenate 'string acc (string c)))
+            (#\\
+             (if backslashed
+                 (setq acc (push-char c acc))
+                 (setq backslashed t)))
+            ((#\Space #\Tab)
+             (if (or inquotes backslashed)
+                 (progn (setq acc (push-char c acc))
+                        (setq backslashed nil))
                  (progn (push acc result)
                         (setq acc ""))))
-            (#\"
-             (setq inquotes (not inquotes)))
-            (otherwise
-              (setq acc (concatenate 'string acc (string c))))))
+            ((#\" #\')
+             (if backslashed
+                 (setq acc (push-char c acc))
+                 (progn (setq inquotes (not inquotes))
+                        (setq backslashed nil))))
+            (t
+             (setq acc (push-char c acc)))))
     (if (not (string= acc ""))
         (push acc result))
     (nreverse result)))
